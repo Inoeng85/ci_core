846cad3f4337544f5f867d565231afeb220310c4
inoeng85 <inoengsyoekoer@gmail.com>
2016-05-30 17:04:17 +0700

    test commit

---
+++ application/views/template.php
@@ -0,0 +1,19 @@
+<div id="container">
+  <h1>Welcome to CodeIgniter!</h1>
+
+  <div id="body">
+    <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+    <p>If you would like to edit this page you'll find it located at:</p>
+    <code>application/views/welcome_message.php</code>
+
+    <p>The corresponding controller for this page is found at:</p>
+    <code>application/controllers/Welcome.php</code>
+
+    <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+  </div>
+
+  <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+
+<div class="body" data-example-id="contextual-table"> <table class="table"> <thead> <tr> <th>#</th> <th>Column heading</th> <th>Column heading</th> <th>Column heading</th> </tr> </thead> <tbody> <tr class="active"> <th scope="row">1</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">2</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="success"> <th scope="row">3</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">4</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="info"> <th scope="row">5</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">6</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="warning"> <th scope="row">7</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr> <th scope="row">8</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> <tr class="danger"> <th scope="row">9</th> <td>Column content</td> <td>Column content</td> <td>Column content</td> </tr> </tbody> </table> </div>
